3b2d990669924a237b558cab24f79b73eea676e6
Matej02 <korda050@seznam.cz>
2024-10-03 12:30:16 +0000

    Sudý Lichý

--- úkol.py
+++ úkol.py
@@ -2,15 +2,8 @@
 def sudy_nebo_lichy(cislo):
     
     if cislo % 2 == 0:
-        print("Číslo je sudé")  # Výpis informace o sudém čísle
+        print("Číslo je sudé") 
     else:
-        # Pokud není zbytek 0, číslo je liché
-        print("Číslo je liché")  # Výpis informace o lichém čísle
-
-# Nyní budeme testovat naši funkci s různými čísly
-
-# Test 1: Zkontrolujeme číslo 3
-sudy_nebo_lichy(3)  # Očekáváme, že to vypíše "Číslo je liché"
-
-# Test 2: Zkontrolujeme číslo 1000000
-sudy_nebo_lichy(1000000)  # Očekáváme, že to vypíše "Číslo je sudé"
+        print("Číslo je liché")
+sudy_nebo_lichy(3)
+sudy_nebo_lichy(1000000) 
