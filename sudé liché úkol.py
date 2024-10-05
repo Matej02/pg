170adc3ab93ce3b91e9dab29b0c67744ef354563
Matej02 <korda050@seznam.cz>
2024-10-05 16:26:06 +0200

    sudé liché úkol.py

--- sudé liché úkol.py
+++ sudé liché úkol.py
@@ -1,10 +1,9 @@
-# Funkce, která určuje, zda je číslo sudé nebo liché
 def sudy_nebo_lichy(cislo):
-    
     if cislo % 2 == 0:
-        print("Číslo je sudé") 
+        print("Číslo", cislo, "je sudé")
     else:
-        print("Číslo je liché")
-sudy_nebo_lichy(3)
-sudy_nebo_lichy(1000000) 
+        print("Číslo", cislo, "je liché")
 
+# Volání funkce
+sudy_nebo_lichy(5)
+sudy_nebo_lichy(1000000)
