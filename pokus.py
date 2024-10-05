a1964fc91fa8a9024b3fac0514c840156cd4efe4
Matej02 <korda050@seznam.cz>
2024-10-05 14:20:53 +0000

    První úkol

---
+++ pokus.py
@@ -0,0 +1,7 @@
+def five_stars():
+    i = 0  
+    while i < 5: 
+        print("*")
+        i += 1  
+
+five_stars()
--- úkol.py
+++
@@ -1,9 +0,0 @@
-# Funkce, která určuje, zda je číslo sudé nebo liché
-def sudy_nebo_lichy(cislo):
-    
-    if cislo % 2 == 0:
-        print("Číslo je sudé") 
-    else:
-        print("Číslo je liché")
-sudy_nebo_lichy(3)
-sudy_nebo_lichy(1000000) 
